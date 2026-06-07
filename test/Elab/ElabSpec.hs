f20af3fee54c0d02a1c60bc63145bbf2c176e3c1
Niek Peters <n.s.peters05@gmail.com>
2026-06-07 23:21:42 +0200

    Cleaned up tests

--- test/Elab/ElabSpec.hs
+++ test/Elab/ElabSpec.hs
@@ -1,9 +1,8 @@
 module Elab.ElabSpec (spec) where
 
 import Test.Hspec
-import TestUtils (fileProcessedShouldBe, shouldBeGolden)
-import Data.List.NonEmpty
-import Elab (ElabResult, elab)
+import TestUtils (shouldBeGolden)
+import Elab (elab)
 import Parser (parse)
 import Text.Show.Pretty (ppShow)
 
@@ -17,7 +16,3 @@ spec =
 shouldElabToGolden :: String -> Spec
 shouldElabToGolden file = it ("correctly elaborates example program " ++ file) $ shouldBeGolden ("test/Elab/" ++ file ++ ".nbm") f
     where   f = ppShow . elab . parse
-
--- -- helper function to make tests more concise
--- shouldElabTo :: String -> ElabResult -> SpecWith (Arg Expectation)
--- shouldElabTo file = it ("correctly elaborates example program " ++ file) . fileProcessedShouldBe ("test/Elab/" ++ file ++ ".nbm") (elab . parse)
