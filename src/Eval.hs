e4cc199b133493715d6761f6e066055950e86921
Niek Peters <n.s.peters05@gmail.com>
2026-06-08 17:57:36 +0200

    Non-hint version is now working

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,47 +1,39 @@
 module Eval (eval, EvalResult) where
 
-import Language.Haskell.Interpreter (runInterpreter, loadModules, setTopLevelModules, interpret, as)
-import Language.Haskell.Interpreter.Unsafe (unsafeRunInterpreterWithArgsLibdir)
 import IR (IRExpr, IR, IREvalResult (IREvalResult))
 import Elab (elabTopLevelExpr, ElabResult)
 import Parser (runExprParser)
 import qualified Data.Map as Map
 import Types (Fragment(..))
-import System.Process (readProcess, readProcessWithExitCode)
+import System.Process (readProcessWithExitCode)
 import GHC.IO.Exception (ExitCode(..))
 
 type EvalResult = [Fragment String IR IREvalResult]
 
 eval :: ElabResult -> FilePath -> String -> [String] -> IO EvalResult
 eval frags lib modName exprs = do
+    -- compileLib lib
     results <- evalExprs lib modName exprs
     let irExprs = map haskellToIR results
     return $ replaceEvalFrags frags irExprs
 
+-- compileLib :: String -> IO ()
+-- compileLib lib = do
+--     (exitCode, _, stderr) <- readProcessWithExitCode "ghc" ["-v0", "-c", lib] ""
+--     return $ case exitCode of
+--         ExitSuccess   -> ()
+--         ExitFailure _ -> error $ "HASKELL COMPILATION FAILED:" ++ stderr
+
+-- NOTE: this function invokes GHC for each expression. In the future it would be better to either pre-compile the module or feed it all expressions as a list and parse the result
 evalExprs :: FilePath -> String -> [String] -> IO [String]
 evalExprs _ _ [] = pure []
-evalExprs lib modName frags = mapM (evalExpr lib) frags 
-    -- error "GG"
-    -- result <- forM frags $ \frag -> do
-
-    -- ghcPath <- getRuntimeGHCPath
-    -- result <- unsafeRunInterpreterWithArgsLibdir [] ghcPath $ do
-    --     loadModules [lib]
-    --     setTopLevelModules [modName]
-    --     let showExprs = map ("show $ " ++) frags
-        
-    --     mapM (`interpret` (as :: String)) showExprs 
-
-    -- case result of
-    --     Right val -> return val
-    --     Left err -> error $ show err
-    where   evalExpr lib expr = do
-                (exitCode, stdout, stderr) <- readProcessWithExitCode "ghc" ["-v0", lib, "-e", expr] ""
+evalExprs lib modName frags = mapM evalExpr frags
+    where   evalExpr expr = do
+                (exitCode, stdout, stderr) <- readProcessWithExitCode "ghc" ["-v0", "-XTypeApplications", lib, "-e", expr] ""
                 return $ case exitCode of
-                    ExitSuccess -> init stdout
+                    ExitSuccess -> init stdout  -- drop tailing newline
                     ExitFailure _ -> error stderr
                     
-
 haskellToIR :: String -> IRExpr
 haskellToIR haskell = elaborated
     where   nbm = haskellToNBM haskell
